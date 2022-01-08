27126023bb23dc3a47b737170663adface4344fb
danielsarabuna <danielsarabuna@gmail.com>
2022-01-09 00:43:28 +0200

    v1.1

--- Program.cs
+++ Program.cs
@@ -4,19 +4,16 @@ using System.Linq;
 
 public class Program
 {  
-  public static string PigIt(string str)
+  public static bool Scramble(string str1, string str2)
   {
-    public static bool Scramble(string str1, string str2)
-    {
-      char[] arr = str2.ToCharArray();
-      List<Char> target = str1.ToCharArray().ToList();
+    char[] arr = str2.ToCharArray();
+    List<Char> target = str1.ToCharArray().ToList();
 
-      for (int i = 0; i < arr.Length; i++)
-      {
-        if (!target.Contains(arr[i])) return false;
-        target.Remove(arr[i]);
-      }
-      return true;
+    for (int i = 0; i < arr.Length; i++)
+    {
+      if (!target.Contains(arr[i])) return false;
+      target.Remove(arr[i]);
     }
+    return true;
   }
 }
